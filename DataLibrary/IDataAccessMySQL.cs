720c7c920156052bb5ec72fd59f922f03bb1812b
auzzzy <v-auzclement@microsoft.com>
2023-03-07 09:01:46 -0800

    Add project files.

---
+++ DataLibrary/IDataAccessMySQL.cs
@@ -0,0 +1,8 @@
+﻿namespace DataLibrary
+{
+    public interface IDataAccessMySQL
+    {
+        Task<List<T>> LoadData<T, U>(string sql, U parameters, string connectionstring);
+        Task SaveData<T>(string sql, T parameters, string connectionstring);
+    }
+}
