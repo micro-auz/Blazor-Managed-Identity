813022e47d95fc8eb8818dadf4afeb550446aaf6
auzzzy <v-auzclement@microsoft.com>
2023-03-07 18:11:05 -0800

    Updated app to use azure keyvault for connectionstring

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -1,7 +1,11 @@
+using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 using BlazorApp1.Data;
 using DataLibrary;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using System.Net.NetworkInformation;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,10 +14,57 @@ builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
 builder.Services.AddSingleton<IDataAccessMySQL, DataAccessMySQL>();
+builder.Configuration.GetSection("ConnectionStrings");
+
+
+if (builder.Environment.IsProduction())
+{
+    System.Diagnostics.Debug.WriteLine("This is production!");
+
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+        new DefaultAzureCredential());
+}
+
+if (builder.Environment.IsDevelopment())
+{
+    System.Diagnostics.Debug.WriteLine("This is development!");
+
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+        new DefaultAzureCredential());
+}
+
+
+
+//Get secrets from Azure Keyvault
+//https://learn.microsoft.com/en-us/azure/key-vault/general/tutorial-net-create-vault-azure-web-app
+SecretClientOptions options = new SecretClientOptions()
+{
+    Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+};
+
+var client = new SecretClient(new Uri("https://kv-rg-auz-2.vault.azure.net/"), new DefaultAzureCredential(), options);
+KeyVaultSecret secret = await client.GetSecretAsync("AppSecret");
+string secretValue = secret.Value;
+System.Diagnostics.Debug.WriteLine("This is the secret:" + secretValue);
+
+//var connectionString = System.Configuration.ConfigurationManager.AppSettings["SqlConnectionString"];
+var connectionString = builder.Configuration.GetConnectionString("SqlConnectionString");
+System.Diagnostics.Debug.WriteLine("This is the connectionString:" + connectionString);
+
+var connectionString2 = builder.Configuration["SqlConnectionString"];
+System.Diagnostics.Debug.WriteLine("This is the connectionString2:" + connectionString2);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline. 
 if (!app.Environment.IsDevelopment())
 {
 	app.UseExceptionHandler("/Error");
