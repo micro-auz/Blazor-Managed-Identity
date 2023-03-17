624369501db899b6f6b744b8bf64f77e246e155d
auzzzy <v-auzclement@microsoft.com>
2023-03-17 13:26:18 -0700

    changes made to connectionstring to use managed identity

--- BlazorApp1/Program.cs
+++ BlazorApp1/Program.cs
@@ -5,7 +5,13 @@ using BlazorApp1.Data;
 using DataLibrary;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.Azure.Services.AppAuthentication;
+using MySql.Data.MySqlClient;
+using MySqlX.XDevAPI;
+using System.Configuration;
 using System.Net.NetworkInformation;
+using static Org.BouncyCastle.Math.EC.ECCurve;
+using ConfigurationManager = System.Configuration.ConfigurationManager;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +23,105 @@ builder.Services.AddSingleton<IDataAccessMySQL, DataAccessMySQL>();
 builder.Configuration.GetSection("ConnectionStrings");
 
 
+
+
+
+
+
+
+
+/*Gettting the Token
+var sqlServerTokenProvider = new AzureServiceTokenProvider();
+var SqlAccessToken = await sqlServerTokenProvider.GetAccessTokenAsync("https://ossrdbms-aad.database.windows.net");
+
+
+//
+// Open a connection to the MySQL server using the access token.
+//
+var mySqlConnectionStringBuilder = new MySqlConnectionStringBuilder
+{
+    Server = "mysql-rg-auz-2-auzw1-dev-eastus2-001.mysql.database.azure.com",
+    Database = "people",
+    UserID = "v-auzclement_microsoft.com#EXT#@StudiosCore.onmicrosoft.com",
+    Password = SqlAccessToken,
+    SslMode = MySqlSslMode.Required,
+
+};
+
+var responseMessage = "";
+
+using (var conn = new MySqlConnection(mySqlConnectionStringBuilder.ConnectionString))
+{
+    System.Diagnostics.Debug.WriteLine("Opening connection using access token...");
+    await conn.OpenAsync();
+
+    using (var command = conn.CreateCommand())
+    {
+        command.CommandText = "SELECT VERSION()";
+
+        using (var reader = await command.ExecuteReaderAsync())
+        {
+            while (await reader.ReadAsync())
+            {
+                System.Diagnostics.Debug.WriteLine("\nConnected to MySQL using managed identity!\n\nMySQL version: " + reader.GetString(0));
+                responseMessage = reader.GetString(0);
+            }
+        }
+    }
+}
+
+System.Diagnostics.Debug.WriteLine("The versions is Azure Database for Mysql is:" + responseMessage);
+
+System.Diagnostics.Debug.WriteLine("The MySQL connectionstring is: " + mySqlConnectionStringBuilder.ConnectionString);
+*/
+
+
+
+
+
+
+/*
+var config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+var connectionStringsSection = (ConnectionStringsSection)config.GetSection("ConnectionStringsSection");
+connectionStringsSection.ConnectionStrings["SqlConnectionString"].ConnectionString = mySqlConnectionStringBuilder.ConnectionString;
+config.Save();
+ConfigurationManager.RefreshSection("ConnectionStringsSection");
+
+*/
+
+
+
+
+if (builder.Environment.IsDevelopment())
+{
+    System.Diagnostics.Debug.WriteLine("This is development!");
+
+    builder.Configuration.AddAzureKeyVault(
+        new Uri($"https://{builder.Configuration["KeyVaultName"]}.vault.azure.net/"),
+        new DefaultAzureCredential());
+}
+
+
+
+
+
+
+
+/*
+//the managfed identity client id
+string userAssignedClientId = "be5cf291-d1af-4578-91eb-f5102001637e";
+
+var credential = new DefaultAzureCredential(new DefaultAzureCredentialOptions { ManagedIdentityClientId = userAssignedClientId });
+
+
+
+System.Diagnostics.Debug.WriteLine("credential: " + credential.);
+*/
+
+
+
+/*
 if (builder.Environment.IsProduction())
 {
     System.Diagnostics.Debug.WriteLine("This is production!");
@@ -61,6 +166,16 @@ System.Diagnostics.Debug.WriteLine("This is the connectionString:" + connectionS
 
 var connectionString2 = builder.Configuration["SqlConnectionString"];
 System.Diagnostics.Debug.WriteLine("This is the connectionString2:" + connectionString2);
+
+*/
+
+
+
+
+
+
+
+
 
 var app = builder.Build();
 
